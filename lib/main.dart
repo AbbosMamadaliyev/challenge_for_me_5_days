c06c8be5a542df42ba85140033bf9b931b0dce2f
Abbos Mamadaliyev <abbbos166@gmail.com>
2022-04-13 13:02:24 +0500

    day-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,14 +18,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
